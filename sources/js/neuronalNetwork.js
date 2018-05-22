02ef09fa6c4f460b68a69fefa61d0c9c261b01c3
Björn Hempel <bjoern@hempel.li>
2018-05-22 23:53:49 +0200

    add calculateOutput function

--- sources/js/neuronalNetwork.js
+++ sources/js/neuronalNetwork.js
@@ -12,21 +12,44 @@ class NeuronalNetwork {
         this.planes = planes;
 
         this.weightMatrices = [];
+        this.inputs = [];
+        this.outputs = [];
 
-        for (var i = 0; i < this.planes.length - 1; i++) {
-            var weightMatrix = [];
+        // for (var i = 0; i < this.planes.length - 1; i++) {
+        //     var weightMatrix = [];
+        //
+        //     for (var j = 0; j < this.planes[i + 1]; j++) {
+        //         weightMatrix[j] = [];
+        //         for (var k = 0; k < this.planes[i]; k++) {
+        //             weightMatrix[j][k] = 1;
+        //         }
+        //     }
+        //
+        //     this.weightMatrices.push(new Matrix(weightMatrix));
+        // }
+        this.weightMatrices.push(new Matrix([[0.9, 0.3, 0.4], [0.2, 0.8, 0.2], [0.1, 0.5, 0.6]]));
+        this.weightMatrices.push(new Matrix([[0.3, 0.7, 0.5], [0.6, 0.5, 0.2], [0.8, 0.1, 0.9]]));
+    }
 
-            for (var j = 0; j < this.planes[i + 1]; j++) {
-                weightMatrix[j] = [];
-                for (var k = 0; k < this.planes[i]; k++) {
-                    weightMatrix[j][k] = 1;
-                }
-            }
+    /**
+     * Calculate the output.
+     *
+     * @param vector {Vector}
+     */
+    calculateOutput(vector) {
+        this.inputs.push(vector);
+        this.outputs.push(this.inputs[0]);
 
-            this.weightMatrices.push(new Matrix(weightMatrix));
+        for (var i = 0; i < this.planes.length; i++) {
+            this.inputs.push(this.weightMatrices[i].multiply(this.outputs[i]));
+
+            var output = this.inputs[i + 1].callback(function (element) {
+                return 1 / (1 + Math.exp(-element));
+            });
+
+            this.outputs.push(output);
         }
 
-        console.log(this.weightMatrices[0].multiply(new Vector([2, 2])).array);
-        console.log(this.weightMatrices[0].multiply(new Vector([1, 1])).array);
+        return this.outputs[this.outputs.length - 1];
     }
 }
