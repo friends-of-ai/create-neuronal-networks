fdae38a0091099fd65f495a6bf309157f324b1ba
Björn Hempel <bjoern@hempel.li>
2018-06-06 01:03:32 +0200

    refactoring single tests

--- tests/js/neuronalNetwork.js
+++ tests/js/neuronalNetwork.js
@@ -36,13 +36,13 @@ function startNeuronalNetworkTest() {
             var neuronalNetwork = new NeuronalNetwork(planes);
 
             return (
-                Test.equalArray(neuronalNetwork.planes, [2, 2, 2]) &&
-                neuronalNetwork.weightMatrices instanceof Array &&
-                neuronalNetwork.weightMatrices.length === 2 &&
-                neuronalNetwork.weightMatrices[0] instanceof Matrix &&
-                Test.equalArray(neuronalNetwork.weightMatrices[0].size, [2, 2]) &&
-                neuronalNetwork.weightMatrices[1] instanceof Matrix &&
-                Test.equalArray(neuronalNetwork.weightMatrices[1].size, [2, 2])
+                Test.equalArrayValues(neuronalNetwork.planes, [2, 2, 2]) &&
+                Test.equalObjectInstance(neuronalNetwork.weightMatrices, Array) &&
+                Test.equalArrayLength(neuronalNetwork.weightMatrices, 2) &&
+                Test.equalObjectInstance(neuronalNetwork.weightMatrices[0], Matrix) &&
+                Test.equalArrayValues(neuronalNetwork.weightMatrices[0].size, [2, 2]) &&
+                Test.equalObjectInstance(neuronalNetwork.weightMatrices[1], Matrix) &&
+                Test.equalArrayValues(neuronalNetwork.weightMatrices[1].size, [2, 2])
             );
         }
     );
@@ -56,13 +56,13 @@ function startNeuronalNetworkTest() {
             var neuronalNetwork = new NeuronalNetwork(planes, true);
 
             return (
-                Test.equalArray(neuronalNetwork.planes, [2, 2, 2]) &&
-                neuronalNetwork.weightMatrices instanceof Array &&
-                neuronalNetwork.weightMatrices.length === 2 &&
-                neuronalNetwork.weightMatrices[0] instanceof Matrix &&
-                Test.equalArray(neuronalNetwork.weightMatrices[0].size, [2, 3]) &&
-                neuronalNetwork.weightMatrices[1] instanceof Matrix &&
-                Test.equalArray(neuronalNetwork.weightMatrices[1].size, [2, 3])
+                Test.equalArrayValues(neuronalNetwork.planes, [2, 2, 2]) &&
+                Test.equalObjectInstance(neuronalNetwork.weightMatrices, Array) &&
+                Test.equalArrayLength(neuronalNetwork.weightMatrices, 2) &&
+                Test.equalObjectInstance(neuronalNetwork.weightMatrices[0], Matrix) &&
+                Test.equalArrayValues(neuronalNetwork.weightMatrices[0].size, [2, 3]) &&
+                Test.equalObjectInstance(neuronalNetwork.weightMatrices[1], Matrix) &&
+                Test.equalArrayValues(neuronalNetwork.weightMatrices[1].size, [2, 3])
             );
         }
     );
@@ -79,13 +79,13 @@ function startNeuronalNetworkTest() {
             var neuronalNetwork = new NeuronalNetwork(weightMatrices);
 
             return (
-                Test.equalArray(neuronalNetwork.planes, [3, 3, 3]) &&
-                neuronalNetwork.weightMatrices instanceof Array &&
-                neuronalNetwork.weightMatrices.length === 2 &&
-                neuronalNetwork.weightMatrices[0] instanceof Matrix &&
-                Test.equalArray(neuronalNetwork.weightMatrices[0].size, [3, 3]) &&
-                neuronalNetwork.weightMatrices[1] instanceof Matrix &&
-                Test.equalArray(neuronalNetwork.weightMatrices[1].size, [3, 3])
+                Test.equalArrayValues(neuronalNetwork.planes, [3, 3, 3]) &&
+                Test.equalObjectInstance(neuronalNetwork.weightMatrices, Array) &&
+                Test.equalArrayLength(neuronalNetwork.weightMatrices, 2) &&
+                Test.equalObjectInstance(neuronalNetwork.weightMatrices[0], Matrix) &&
+                Test.equalArrayValues(neuronalNetwork.weightMatrices[0].size, [3, 3]) &&
+                Test.equalObjectInstance(neuronalNetwork.weightMatrices[1], Matrix) &&
+                Test.equalArrayValues(neuronalNetwork.weightMatrices[1].size, [3, 3])
             );
         }
     );
@@ -102,15 +102,15 @@ function startNeuronalNetworkTest() {
             var neuronalNetwork = new NeuronalNetwork(weightMatrices, true);
 
             return (
-                Test.equalArray(neuronalNetwork.planes, [2, 2, 2]) &&
-                neuronalNetwork.weightMatrices instanceof Array &&
-                neuronalNetwork.weightMatrices.length === 2 &&
-                neuronalNetwork.weightMatrices[0] instanceof Matrix &&
-                Test.equalArray(neuronalNetwork.weightMatrices[0].array, [[0.3, 0.8, 0.5], [-0.2, -0.6, 0.7]]) &&
-                Test.equalArray(neuronalNetwork.weightMatrices[0].size, [2, 3]) &&
-                neuronalNetwork.weightMatrices[1] instanceof Matrix &&
-                Test.equalArray(neuronalNetwork.weightMatrices[1].array, [[0.2, 0.4, 0.3], [0.1, -0.4, 0.9]]) &&
-                Test.equalArray(neuronalNetwork.weightMatrices[1].size, [2, 3])
+                Test.equalArrayValues(neuronalNetwork.planes, [2, 2, 2]) &&
+                Test.equalObjectInstance(neuronalNetwork.weightMatrices, Array) &&
+                Test.equalArrayLength(neuronalNetwork.weightMatrices, 2) &&
+                Test.equalObjectInstance(neuronalNetwork.weightMatrices[0], Matrix) &&
+                Test.equalArrayValues(neuronalNetwork.weightMatrices[0].array, [[0.3, 0.8, 0.5], [-0.2, -0.6, 0.7]]) &&
+                Test.equalArrayValues(neuronalNetwork.weightMatrices[0].size, [2, 3]) &&
+                Test.equalObjectInstance(neuronalNetwork.weightMatrices[1], Matrix) &&
+                Test.equalArrayValues(neuronalNetwork.weightMatrices[1].array, [[0.2, 0.4, 0.3], [0.1, -0.4, 0.9]]) &&
+                Test.equalArrayValues(neuronalNetwork.weightMatrices[1].size, [2, 3])
             );
         }
     );
@@ -146,13 +146,13 @@ function startNeuronalNetworkTest() {
             var output = neuronalNetwork.calculateOutput(new Vector([0.9, 0.1, 0.8]));
 
             return (
-                Test.equalArray(neuronalNetwork.planes, [3, 3, 3]) &&
-                neuronalNetwork.weightMatrices instanceof Array &&
-                neuronalNetwork.weightMatrices.length === 2 &&
-                neuronalNetwork.weightMatrices[0] instanceof Matrix &&
-                Test.equalArray(neuronalNetwork.weightMatrices[0].size, [3, 3]) &&
-                neuronalNetwork.weightMatrices[1] instanceof Matrix &&
-                Test.equalArray(neuronalNetwork.weightMatrices[1].size, [3, 3]) &&
+                Test.equalArrayValues(neuronalNetwork.planes, [3, 3, 3]) &&
+                Test.equalObjectInstance(neuronalNetwork.weightMatrices, Array) &&
+                Test.equalArrayLength(neuronalNetwork.weightMatrices, 2) &&
+                Test.equalObjectInstance(neuronalNetwork.weightMatrices[0], Matrix) &&
+                Test.equalArrayValues(neuronalNetwork.weightMatrices[0].size, [3, 3]) &&
+                Test.equalObjectInstance(neuronalNetwork.weightMatrices[1], Matrix) &&
+                Test.equalArrayValues(neuronalNetwork.weightMatrices[1].size, [3, 3]) &&
                 Test.equalNumber(output.getCell(0), 0.7263033450139793, 6) &&
                 Test.equalNumber(output.getCell(1), 0.7085980724248232, 6) &&
                 Test.equalNumber(output.getCell(2), 0.778097059561142, 6)
@@ -191,15 +191,15 @@ function startNeuronalNetworkTest() {
             var output = neuronalNetwork.calculateOutput(new Vector([0.7, 0.6]));
 
             return (
-                Test.equalArray(neuronalNetwork.planes, [2, 2, 2]) &&
-                neuronalNetwork.weightMatrices instanceof Array &&
-                neuronalNetwork.weightMatrices.length === 2 &&
-                neuronalNetwork.weightMatrices[0] instanceof Matrix &&
-                Test.equalArray(neuronalNetwork.weightMatrices[0].array, [[0.3, 0.8, 0.5], [-0.2, -0.6, 0.7]]) &&
-                Test.equalArray(neuronalNetwork.weightMatrices[0].size, [2, 3]) &&
-                neuronalNetwork.weightMatrices[1] instanceof Matrix &&
-                Test.equalArray(neuronalNetwork.weightMatrices[1].array, [[0.2, 0.4, 0.3], [0.1, -0.4, 0.9]]) &&
-                Test.equalArray(neuronalNetwork.weightMatrices[1].size, [2, 3]) &&
+                Test.equalArrayValues(neuronalNetwork.planes, [2, 2, 2]) &&
+                Test.equalObjectInstance(neuronalNetwork.weightMatrices, Array) &&
+                Test.equalArrayLength(neuronalNetwork.weightMatrices, 2) &&
+                Test.equalObjectInstance(neuronalNetwork.weightMatrices[0], Matrix) &&
+                Test.equalArrayValues(neuronalNetwork.weightMatrices[0].array, [[0.3, 0.8, 0.5], [-0.2, -0.6, 0.7]]) &&
+                Test.equalArrayValues(neuronalNetwork.weightMatrices[0].size, [2, 3]) &&
+                Test.equalObjectInstance(neuronalNetwork.weightMatrices[1], Matrix) &&
+                Test.equalArrayValues(neuronalNetwork.weightMatrices[1].array, [[0.2, 0.4, 0.3], [0.1, -0.4, 0.9]]) &&
+                Test.equalArrayValues(neuronalNetwork.weightMatrices[1].size, [2, 3]) &&
                 Test.equalNumber(output.getCell(0), 0.6546591654993577, 6) &&
                 Test.equalNumber(output.getCell(1), 0.5499865411657244, 6)
             );
@@ -223,27 +223,27 @@ function startNeuronalNetworkTest() {
             var trainResult = neuronalNetwork.train(input, expected);
 
             return (
-                Test.equalArray(neuronalNetwork.planes, [2, 2, 2]) &&
-                neuronalNetwork.weightMatrices instanceof Array &&
-                neuronalNetwork.weightMatrices.length === 2 &&
-                neuronalNetwork.weightMatrices[0] instanceof Matrix &&
-                Test.equalArray(neuronalNetwork.weightMatrices[0].array, [[0.31032731958698523, 0.8072291237108897, 0.5061963917521911], [-0.2151776888223207, -0.6106243821756244, 0.6908933867066076]]) &&
-                Test.equalArray(neuronalNetwork.weightMatrices[0].size, [2, 3]) &&
-                neuronalNetwork.weightMatrices[1] instanceof Matrix &&
-                Test.equalArray(neuronalNetwork.weightMatrices[1].array, [[0.2554667889679425, 0.4422286810285825, 0.324969262671611], [0.013377860083794227, -0.4659482689479074, 0.8610056575296824]]) &&
-                Test.equalArray(neuronalNetwork.weightMatrices[1].size, [2, 3]) &&
-                trainResult.backPropagation.delta instanceof Array &&
-                trainResult.backPropagation.delta.length == 2 &&
-                trainResult.backPropagation.delta[0] instanceof Vector &&
-                Test.equalArray(trainResult.backPropagation.delta[0].array, [0.010327319586985247, -0.015177688822320668]) &&
-                trainResult.backPropagation.delta[1] instanceof Vector &&
-                Test.equalArray(trainResult.backPropagation.delta[1].array, [0.05546678896794252, -0.08662213991620578]) &&
-                trainResult.backPropagation.weightMatrixDelta instanceof Array &&
-                trainResult.backPropagation.weightMatrixDelta.length == 2 &&
-                trainResult.backPropagation.weightMatrixDelta[0] instanceof Matrix &&
-                Test.equalArray(trainResult.backPropagation.weightMatrixDelta[0].array, [[0.010327319586985247, 0.007229123710889672, 0.006196391752191148], [-0.015177688822320668, -0.010624382175624466, -0.0091066132933924]]) &&
-                trainResult.backPropagation.weightMatrixDelta[1] instanceof Matrix &&
-                Test.equalArray(trainResult.backPropagation.weightMatrixDelta[1].array, [[0.05546678896794252, 0.042228681028582474, 0.024969262671611037], [-0.08662213991620578, -0.06594826894790738, -0.03899434247031761]])
+                Test.equalArrayValues(neuronalNetwork.planes, [2, 2, 2]) &&
+                Test.equalObjectInstance(neuronalNetwork.weightMatrices, Array) &&
+                Test.equalArrayLength(neuronalNetwork.weightMatrices, 2) &&
+                Test.equalObjectInstance(neuronalNetwork.weightMatrices[0], Matrix) &&
+                Test.equalArrayValues(neuronalNetwork.weightMatrices[0].array, [[0.31032731958698523, 0.8072291237108897, 0.5061963917521911], [-0.2151776888223207, -0.6106243821756244, 0.6908933867066076]]) &&
+                Test.equalArrayValues(neuronalNetwork.weightMatrices[0].size, [2, 3]) &&
+                Test.equalObjectInstance(neuronalNetwork.weightMatrices[1], Matrix) &&
+                Test.equalArrayValues(neuronalNetwork.weightMatrices[1].array, [[0.2554667889679425, 0.4422286810285825, 0.324969262671611], [0.013377860083794227, -0.4659482689479074, 0.8610056575296824]]) &&
+                Test.equalArrayValues(neuronalNetwork.weightMatrices[1].size, [2, 3]) &&
+                Test.equalObjectInstance(trainResult.backPropagation.delta, Array) &&
+                Test.equalArrayLength(trainResult.backPropagation.delta, 2) &&
+                Test.equalObjectInstance(trainResult.backPropagation.delta[0], Vector) &&
+                Test.equalArrayValues(trainResult.backPropagation.delta[0].array, [0.010327319586985247, -0.015177688822320668]) &&
+                Test.equalObjectInstance(trainResult.backPropagation.delta[1], Vector) &&
+                Test.equalArrayValues(trainResult.backPropagation.delta[1].array, [0.05546678896794252, -0.08662213991620578]) &&
+                Test.equalObjectInstance(trainResult.backPropagation.weightMatrixDelta, Array) &&
+                Test.equalArrayLength(trainResult.backPropagation.weightMatrixDelta, 2) &&
+                Test.equalObjectInstance(trainResult.backPropagation.weightMatrixDelta[0], Matrix) &&
+                Test.equalArrayValues(trainResult.backPropagation.weightMatrixDelta[0].array, [[0.010327319586985247, 0.007229123710889672, 0.006196391752191148], [-0.015177688822320668, -0.010624382175624466, -0.0091066132933924]]) &&
+                Test.equalObjectInstance(trainResult.backPropagation.weightMatrixDelta[1], Matrix) &&
+                Test.equalArrayValues(trainResult.backPropagation.weightMatrixDelta[1].array, [[0.05546678896794252, 0.042228681028582474, 0.024969262671611037], [-0.08662213991620578, -0.06594826894790738, -0.03899434247031761]])
             );
         }
     );
@@ -268,7 +268,7 @@ function startNeuronalNetworkTest() {
             var output = neuronalNetwork.calculateOutput(input);
 
             return (
-                Test.equalArray(neuronalNetwork.planes, [2, 2, 2]) &&
+                Test.equalArrayValues(neuronalNetwork.planes, [2, 2, 2]) &&
                 Test.equalObjectInstance(output, Vector) &&
                 Test.equalInteger(output.size, 2) &&
                 Test.equalNumber(output.getCell(0), expected.getCell(0), 1) &&
